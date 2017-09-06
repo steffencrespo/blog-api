1cdca2b6b4f36a0d6eb5fdbcabb380b46aac44c7
Leonardo Steffen <leosteffen84@gmail.com>
2017-09-05 20:49:47 -0400

    adds post route

--- blogPostsRouter.js
+++ blogPostsRouter.js
@@ -14,8 +14,20 @@ router.get('/', (req, res) => {
 	res.json(BlogPosts.get());
 });
 
-router.post('/', (req, res) => {
+router.post('/', jsonParser, (req, res) => {
+	const requiredFields = ['title', 'content', 'author', 'publishDate'];
 
+	for (let i = 0; i < requiredFields.length; i++) {
+		const field = requiredFields[i];
+		if (!(field in req.body)) {
+			const error = `Missing ${field} in request body`;
+			console.error(error);
+			return res.status(400).send(error);
+		}
+	}
+
+	const item = BlogPosts.create(req.body.title, req.body.content, req.body.author, req.body.publishDate);
+	return res.status(200).send(item);
 });
 
 router.put('/:id', (req, res) => {
