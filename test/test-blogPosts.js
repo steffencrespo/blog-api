e506cce5b8768ac2757d1474396d3a429d23e3f6
Leonardo Steffen <leosteffen84@gmail.com>
2017-09-09 19:15:35 -0400

    adds test for blog post create

--- test/test-blogPosts.js
+++ test/test-blogPosts.js
@@ -24,4 +24,23 @@ describe('blog posts', function(){
 				});
 	});
 
+	it('should create a new post', function() {
+		let newPost = {'title':'test post', 'content': 'dummy content', 'author': 'test bot', 'publishDate': 'today'};
+		return chai.request(app)
+				.post('/blog-posts')
+				.send(newPost)
+				.then(function(res){
+					res.should.have.status(201);
+					res.body.should.include.keys('title', 'content', 'author', 'publishDate');
+					res.body.should.deep.equal({'id': res.body.id, 'title':'test post', 'content': 'dummy content', 'author': 'test bot', 'publishDate': 'today'})
+				});
+	});
+
+	it('should modify an existing post', function() {
+
+	});
+
+	it('should delete a post', function() {
+
+	})
 });
